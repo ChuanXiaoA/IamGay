26b095401f121ddb05b719753cc1af57c1b6863f
2018aoaojiao <754629697@qq.com>
2018-04-24 18:49:45 +0800

    钟

---
+++ MyZhong/app/src/main/java/gitstudy/com/bw/myzhong/fragment/F3.java
@@ -0,0 +1,23 @@
+package gitstudy.com.bw.myzhong.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import gitstudy.com.bw.myzhong.R;
+
+/**
+ * Created by lenovo on 2018/4/23.
+ */
+
+public class F3 extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.f3,container,false);
+
+        return view;
+    }
+}
---
+++ MyZhong/app/src/main/java/gitstudy/com/bw/myzhong/fragment/f1/F12.java
@@ -0,0 +1,22 @@
+package gitstudy.com.bw.myzhong.fragment.f1;
+
+import android.view.View;
+
+import gitstudy.com.bw.myzhong.R;
+import gitstudy.com.bw.myzhong.base.BaseFragment;
+
+/**
+ * Created by lenovo on 2018/4/23.
+ */
+
+public class F12 extends BaseFragment {
+    @Override
+    protected void initView(View view) {
+
+    }
+
+    @Override
+    protected int getFid() {
+        return R.layout.f12;
+    }
+}
