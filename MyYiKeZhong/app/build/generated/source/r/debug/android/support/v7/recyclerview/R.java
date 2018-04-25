4a06595b08ddee421fe6c3bae05d6e787427746b
xinzang1 <1250897947@qq.com>
2018-04-25 14:04:40 +0800

    yz6

--- MyYiKeZhong/app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
+++ MyYiKeZhong/app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
@@ -8,29 +8,29 @@ package android.support.v7.recyclerview;
 
 public final class R {
     public static final class attr {
-        public static final int fastScrollEnabled = 0x7f020095;
-        public static final int fastScrollHorizontalThumbDrawable = 0x7f020096;
-        public static final int fastScrollHorizontalTrackDrawable = 0x7f020097;
-        public static final int fastScrollVerticalThumbDrawable = 0x7f020098;
-        public static final int fastScrollVerticalTrackDrawable = 0x7f020099;
-        public static final int layoutManager = 0x7f0200bf;
-        public static final int reverseLayout = 0x7f020135;
-        public static final int spanCount = 0x7f02014e;
-        public static final int stackFromEnd = 0x7f020154;
+        public static final int fastScrollEnabled = 0x7f030096;
+        public static final int fastScrollHorizontalThumbDrawable = 0x7f030097;
+        public static final int fastScrollHorizontalTrackDrawable = 0x7f030098;
+        public static final int fastScrollVerticalThumbDrawable = 0x7f030099;
+        public static final int fastScrollVerticalTrackDrawable = 0x7f03009a;
+        public static final int layoutManager = 0x7f0300c7;
+        public static final int reverseLayout = 0x7f03013d;
+        public static final int spanCount = 0x7f030157;
+        public static final int stackFromEnd = 0x7f03015d;
     }
     public static final class dimen {
-        public static final int fastscroll_default_thickness = 0x7f050074;
-        public static final int fastscroll_margin = 0x7f050075;
-        public static final int fastscroll_minimum_range = 0x7f050076;
-        public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f05007e;
-        public static final int item_touch_helper_swipe_escape_max_velocity = 0x7f05007f;
-        public static final int item_touch_helper_swipe_escape_velocity = 0x7f050080;
+        public static final int fastscroll_default_thickness = 0x7f060074;
+        public static final int fastscroll_margin = 0x7f060075;
+        public static final int fastscroll_minimum_range = 0x7f060076;
+        public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f06007e;
+        public static final int item_touch_helper_swipe_escape_max_velocity = 0x7f06007f;
+        public static final int item_touch_helper_swipe_escape_velocity = 0x7f060080;
     }
     public static final class id {
-        public static final int item_touch_helper_previous_elevation = 0x7f070063;
+        public static final int item_touch_helper_previous_elevation = 0x7f08006e;
     }
     public static final class styleable {
-        public static final int[] RecyclerView = { 0x010100c4, 0x010100f1, 0x7f020095, 0x7f020096, 0x7f020097, 0x7f020098, 0x7f020099, 0x7f0200bf, 0x7f020135, 0x7f02014e, 0x7f020154 };
+        public static final int[] RecyclerView = { 0x010100c4, 0x010100f1, 0x7f030096, 0x7f030097, 0x7f030098, 0x7f030099, 0x7f03009a, 0x7f0300c7, 0x7f03013d, 0x7f030157, 0x7f03015d };
         public static final int RecyclerView_android_orientation = 0;
         public static final int RecyclerView_android_descendantFocusability = 1;
         public static final int RecyclerView_fastScrollEnabled = 2;
--- MyYiKeZhong/app/src/main/java/com/bwei/myyikezhong/Fragment/f1/F11.java
+++ MyYiKeZhong/app/src/main/java/com/bwei/myyikezhong/Fragment/f1/F11.java
@@ -8,16 +8,29 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.bwei.myyikezhong.R;
+import com.youth.banner.Banner;
 
 /**
  * Created by lenovo on 2018/4/23.
  */
 
-public class F11 extends Fragment{
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.f11,container,false);
+public class F11 extends Fragment {
+    private View view;
+    private Banner mF11Banner;
 
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.f11, container, false);
+
+
+
+
+
+
+        initView(view);
         return view;
     }
 
+    private void initView(View view) {
+        mF11Banner = (Banner) view.findViewById(R.id.f11_Banner);
+    }
 }
