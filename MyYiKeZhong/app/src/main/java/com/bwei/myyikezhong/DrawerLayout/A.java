121fea5106663a63eff71cba30cd2b0f51088d28
xinzang1 <1250897947@qq.com>
2018-04-25 08:12:31 +0800

    一刻钟

---
+++ MyYiKeZhong/app/src/main/java/com/bwei/myyikezhong/DrawerLayout/A.java
@@ -0,0 +1,8 @@
+package com.bwei.myyikezhong.DrawerLayout;
+
+/**
+ * Created by lenovo on 2018/4/24.
+ */
+
+public class A {
+}
---
+++ MyYiKeZhong/app/src/main/java/com/bwei/myyikezhong/Fragment/Fragment2.java
@@ -0,0 +1,23 @@
+package com.bwei.myyikezhong.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bwei.myyikezhong.R;
+
+/**
+ * Created by lenovo on 2018/4/23.
+ */
+
+public class Fragment2 extends Fragment {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment2,container,false);
+
+        return view;
+    }
+
+}
